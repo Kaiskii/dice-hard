bbb5d438bc2f01c64da82711320b357914df0aef
Kaiskii <24761071+Kaiskii@users.noreply.github.com>
2022-07-16 03:08:10 +0800

    feat: add kai utils

---
+++ Assets/Scripts/Utils/Singleton.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+public abstract class Singleton<T> : MonoBehaviour where T :Singleton<T>
+{
+    public static T Instance { get; private set; }
+
+    void Awake () {
+        /***
+        * Enforce Singleton Pattern described above.
+        ***/
+        if(Instance != this && Instance != null)
+        {
+            Destroy(gameObject);
+        }
+
+        Instance = (T)this;
+        Setup();
+    }
+
+    protected virtual void Setup()
+    {
+
+    }
+}
