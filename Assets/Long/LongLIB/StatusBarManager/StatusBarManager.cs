8c3097b0ed66bcd512d82d3299a6eee9128171d9
Long YM <lymprivateacc@gmail.com>
2022-07-17 02:36:51 +0800

    Added statusbarManager

--- Assets/Long/LongLIB/StatusBarManager/StatusBarManager.cs
+++ Assets/Long/LongLIB/StatusBarManager/StatusBarManager.cs
@@ -2,19 +2,56 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StatusBarManager : MonoBehaviour
+public class StatusBarManager : Singleton<StatusBarManager>
 {
+    List<StatusBarSO> statusBarList = new List<StatusBarSO>();
+
     // Start is called before the first frame update
     void Start()
     {
         //GET ALL ASSETS
-        List<StatusBarSO> allResources = new List<StatusBarSO>();
-        allResources.AddRange(ResourceIndex.GetAllAssets<StatusBarSO>());
+        statusBarList.AddRange(ResourceIndex.GetAllAssets<StatusBarSO>());
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    #region UI_EVENT_CALLS
+    void OnStatusBarCurrentValueUpdated(string name,float newVal){
 
+    }
+
+    void OnStatusBarMaxValueUpdated(string name,float newVal){
+
+    }
+    #endregion
+
+    public StatusBarSO GetStatusBar(string name)
+    {
+        for(int i = 0;i<statusBarList.Count;i++){
+            if(statusBarList[i].GetBarName() == name){
+                return statusBarList[i];
+            }
+        }
+        return null;
+    }
+
+    public void SetStatusBarMaxValue(string name, float val){
+        StatusBarSO statusBar = GetStatusBar(name);
+        if(!statusBar){
+            Debug.LogWarning("Could not find status bar " + name + "!");
+            return;
+        }
+
+        statusBar.SetBarMaxValue(val);
+        OnStatusBarMaxValueUpdated(name,statusBar.GetBarMaxValue());
+    }
+
+    public void SetStatusBarCurrentValue(string name, float val){
+        StatusBarSO statusBar = GetStatusBar(name);
+        if(!statusBar){
+            Debug.LogWarning("Could not find status bar " + name + "!");
+            return;
+        }
+
+        statusBar.SetBarCurrentValue(val);
+        OnStatusBarCurrentValueUpdated(name,statusBar.GetBarCurrentValue());
     }
 }
